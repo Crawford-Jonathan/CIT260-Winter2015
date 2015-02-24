77787f8ade63ee5557dc657602327e8fc9f66ef2
Jonathan Crawford <johny_rock_360@hotmail.com>
2015-02-24 09:51:45 -0700

    Partial InventoryMenuView

---
+++ WW2-Zombies/src/byui/cit260/ww2Zombies/view/InventoryMenuView.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.ww2Zombies.view;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class InventoryMenuView {
+    private final String MENU = "\n"
+            +"\n-------------------------------------"
+            +"\n| Inventory Menu                    |"
+            +"\n-------------------------------------"
+            +"\nP - Pick up Item"
+            +"\nD - Drop Item"
+            +"\nU - Use Item"
+            +"\nM - Veiw Map"
+            +"\nE - Exit to Game"
+            +"\nR - Return to Previous Menu"
+            +"\n-------------------------------------";
+            
+}
